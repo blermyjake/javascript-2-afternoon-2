000c2dfb08e6c07d72751fc86c849f9eb844d83d
blermyjake <jeremy73120@gmail.com>
2018-06-19 14:55:10 -0500

    1-5

--- practice.js
+++ practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr){
+  return arr[0];
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,6 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
+function last(arr){
+  return arr[arr.length-1];
+}
 
 
 
@@ -48,6 +53,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
+function looper(arr){
+  for(let i = 0; i<arr.length; i++){
+    alert(arr[i])
+  }
+}
 
 
 
@@ -63,7 +73,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(arr){
+  for(let i = arr.length -1; i>=0; i--){
+    alert(arr[i])
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -71,13 +85,22 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 // Do not edit the code below.
 var nums = [1,2,3,6,22,98,45,23,22,12];
 // Do not edit the code above.
-
 /*
   Write a function named evenFinder that is given nums as it's only argument.
   Return an array that contains the even numbers from the nums array.
 */
-
 //Code Here
+function evenFinder(nums){
+  var evensNums = [];
+  for(let i = 0; i<nums.length; i++){
+    if(nums[i]%2===0){
+      evensNums.push(nums[i]);
+    }
+  }
+  return evensNums;
+}
+evenFinder(nums);
+
 
 
 
